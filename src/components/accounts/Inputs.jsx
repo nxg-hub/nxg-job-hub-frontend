ff55f64e7958ee6e3eb8d0a9d5a78b36c59e9dbc
josephnma <josgolf@yahoo.ca>
2023-10-06 18:39:03 +0100

    updated commits to address merge conflict

---
+++ src/components/accounts/Inputs.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Inputs = ({ title, type, value, onChange, errormessage, pattern, placeholder }) => {
+  const [focused, setFocused] = useState(false);
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  }
+
+  return (
+    <div className="inputs">
+      <label>{title}</label>
+      <input
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        pattern={pattern}
+        onBlur={handleFocus}
+        focused={focused.toString()}
+      />
+      <p>{errormessage}</p>
+    </div>
+  );
+};
+
+export default Inputs;
