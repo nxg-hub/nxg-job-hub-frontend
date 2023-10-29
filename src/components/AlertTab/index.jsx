3f4d900f90c9bc34625f0ca20cfead2d2d3e477f
Adele <adelewisdom@gmail.com>
2023-10-29 10:40:21 +0100

    added chatlist into chatPage

--- src/components/AlertTab/index.jsx
+++ src/components/AlertTab/index.jsx
@@ -4,30 +4,33 @@ import s from "./index.module.scss";
 import { BsArrowLeft } from "react-icons/bs";
 import { CiMenuKebab } from "react-icons/ci";
 import { ReactComponent as Search } from "../../../src/static/icons/round-search.svg";
+
 const AlertTab = ({ items, controls, ...props }) => {
-  const { setShowAlertTab, showAlertTab } = controls;
   const [search, setSearch] = useState("");
-  const CloseTab = (e) => {
-    if (e.target === e.currentTarget) setShowAlertTab(!showAlertTab);
-  };
+
   const handleSearch = () => {};
   return (
-    <div title="close" onClick={CloseTab} className={s.AlertTabWrapper}>
+    <div
+      title="close"
+      onClick={controls}
+      className={s.AlertTabWrapper}
+      {...props}
+    >
       <div title="" className={s.AlertTab}>
-      <span>
-        <BsArrowLeft onClick={()=>setShowAlertTab(!showAlertTab)} />
-        <div className={s.searchBar}>
-          <input
-            className={s.searchInput}
-            type="search"
-            placeholder={"Search"}
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <Search onClick={handleSearch} />
-        </div>
+        <span>
+          <BsArrowLeft onClick={controls} />
+          <div className={s.searchBar}>
+            <input
+              className={s.searchInput}
+              type="search"
+              placeholder={"Search"}
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <Search onClick={handleSearch} />
+          </div>
           <CiMenuKebab />
-      </span>
+        </span>
         {items.map((item, id) => (
           <AlertTabItem item={item} key={id} />
         ))}
--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -1,15 +1,51 @@
 import s from "./index.module.scss";
 import chat_data from "../../utils/data/chats";
 import TextBubble from "../../components/TextBubble";
+import AlertTab from "../../components/AlertTab";
+import notification_data from "../../utils/data/notifications";
+import { useNavigate } from "react-router-dom";
+import profilepic from "../../static/images/Peter.png";
+import { ReactComponent as VideoCall } from "../../static/icons/video_call.svg";
+import { ReactComponent as AudioCall } from "../../static/icons/audio_call.svg";
+import { CiMenuKebab } from "react-icons/ci";
+import { PiMicrophoneDuotone, PiCameraLight, PiSmileyLight, PiPaperclipLight } from "react-icons/pi";
+import { useState } from "react";
+
 const Chat = () => {
   const { chat_name, chats } = chat_data;
-  console.log("now", Date.now());
+  const redirect = useNavigate();
+  const Back = (e) => {
+    if (e.target === e.currentTarget) {
+      console.log("redirected");
+      return redirect(-1);
+    }
+  };
+  const [message, SetMessage] = useState("");
+  const handleTyping = (e) => {
+    const { value } = e.target;
+    SetMessage(value)
+  };
   return (
     <div className={s.ChatPage}>
-      <div></div>
+      <div className={s.Messages}>
+        <AlertTab
+          className={s.chats}
+          items={notification_data}
+          controls={Back}
+        />
+      </div>
       <div className={s.ChatScreen}>
         <div className={s.ChatHeader}>
-          <h2>{chat_name}</h2>
+          <div>
+            {" "}
+            <img src={profilepic} alt="" />
+            <h2>{chat_name}</h2>
+          </div>
+          <div>
+            <VideoCall title="Video call" />
+            <AudioCall title="Audio call" />
+            <CiMenuKebab title="More" />
+          </div>
         </div>
         <div className={s.Chats}>
           {chats.map((chat, id) => (
@@ -20,6 +56,25 @@ const Chat = () => {
               timestamp={chat.timestamp}
             />
           ))}
+        </div>
+        <div className={s.SendMessage}>
+          <div className={s.MessageBoxWrapper}>
+            <PiSmileyLight title="Emoji" />
+            <textarea
+              className={s.MessageBox}
+              type="text"
+              placeholder="Type message"
+              value={message}
+              onChange={handleTyping}
+              rows={1}
+              rowspan={30}
+            />
+            <span className={s.features}>
+              <PiPaperclipLight title="Document" />
+              <PiCameraLight title="Camera" />
+              <PiMicrophoneDuotone title="Microphone" />
+            </span>
+          </div>
         </div>
       </div>
     </div>
