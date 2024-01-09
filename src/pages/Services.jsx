b115d4a8ca40d48f0d31b7d735874dbb2f6fe777
Ogochukwu-Ugorji <user.email: "ogochukwu.ugorji@nxghubdigitech.onmicrosoft.com>
2024-01-09 08:42:52 +0000

    Tried fixing landing page job card slider issue

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import EmployerDashProfile from './Dashboard/Employer/routes/EmployerDashProfile/EmployerDashProfile'
 
 const Services = () => {
   return (
     <div>
       Services
+      <EmployerDashProfile/>
     </div>
   )
 }
