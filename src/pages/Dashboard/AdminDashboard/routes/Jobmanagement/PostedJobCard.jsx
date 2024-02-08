c777010fc27f6109b2aa98c52b7667ef543b724c
Ogochukwu-Ugorji <user.email: "ogochukwu.ugorji@nxghubdigitech.onmicrosoft.com>
2024-02-08 16:13:29 +0000

    Working on job management styling

--- src/pages/Dashboard/AdminDashboard/routes/Jobmanagement/PostedJobCard.jsx
+++ src/pages/Dashboard/AdminDashboard/routes/Jobmanagement/PostedJobCard.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './jobmanagement.scss'
 
 export const PostedJobCard = ({jobsToBeVetted}) => {
   return (
     <div className='job-posted'>
         {jobsToBeVetted.map((job) => (
             <div className="job-post-section" key={job.id}>
-                <div className="jobs-contents">
-                    <div className="job-employer-container">
-                        <div className="job-poster">
-                            <img src={job.employerPics} alt={job.employerName} />
-                        </div>
-                        <div className="job-poster-detail">
-                            <h4>{job.employerName}</h4>
-                            <p>{job.userType}</p>
-                        </div>
+                <div className="job-employer-container">
+                    <div className="job-poster">
+                        <img src={job.employerPics} alt={job.employerName} />
                     </div>
+                    <div className="job-poster-detail">
+                        <h4>{job.employerName}</h4>
+                        <p>{job.userType}</p>
+                    </div>
+                </div>
+                <div className="jobs-contents">
                     <div className="posted-job-container">
                         <h5>
                             Job Category: <span>{job.jobCategory}</span>
@@ -29,11 +30,12 @@ export const PostedJobCard = ({jobsToBeVetted}) => {
                         </div>
                         <Link>{job.detailLink}</Link>
                     </div>
+                    <div className="job-posted-btn">
+                        <button>Accept</button>
+                        <button className='decline-btn'>Decline</button>
+                    </div>
                 </div>
-                <div className="job-posted-btn">
-                    <button>Accept</button>
-                    <button className='decline-btn'>Decline</button>
-                </div>
+                
             </div>
         ))}
     </div>
