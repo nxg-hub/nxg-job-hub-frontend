e07f714071d0e16dfa7ca8e4dba4d936a3b74649
Nextgenhub <143799446+nxghub@users.noreply.github.com>
2024-02-13 15:32:56 +0000

    fix file uploadImage

--- server/uploadImage.js
+++ server/uploadImage.js
@@ -1,10 +1,10 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
-    secure: true
+  cloud_name: "dildznazt",
+  api_key: "236923818157321",
+  api_secret: "-Nc3-tS4BZQcdDPvqGJjHS-4G5E",
+  secure: true
 });
 
 const opts = {
