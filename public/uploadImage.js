ef506a68267cf56a63268212a6ce11a45539da14
Nextgenhub <143799446+nxghub@users.noreply.github.com>
2024-02-13 14:58:06 +0000

    try cloudinary config restructure

--- public/uploadImage.js
+++ public/uploadImage.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
+    cloud_name: "dildznazt",
+    api_key: "236923818157321",
+    api_secret: "-Nc3-tS4BZQcdDPvqGJjHS-4G5E",
     secure: true
 });
 
