84900bc4f6f5262c539ded3bd3181b314fa8eccd
Ogochukwu-Ugorji <user.email: "ogochukwu.ugorji@nxghubdigitech.onmicrosoft.com>
2024-03-18 17:38:34 +0000

    Worked on the tech-talen  subscription page to change the subscription price to Naira if the user is a Nigerian

--- src/utils/routers/ProtectedRoute.jsx
+++ src/utils/routers/ProtectedRoute.jsx
@@ -2,15 +2,6 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import isLoggedIn  from '../hooks/isLoggedIn';
 
-// function ProtectedRoute() {
-
-//   return (
-//     <div>
-//       {isLoggedIn() ? <Outlet /> : <Navigate to="/register" />}
-//     </div>
-//   );
-// }
-
 function ProtectedRoute() {
   if (!isLoggedIn()) {
     // Redirect to the login page if not logged in
