f490224ffbfe3693ac9ab59613f7fdad6e0ccd4e
Joseph Nma <josgolf@yahoo.ca>
2024-08-10 13:55:07 +0100

    fix to dashboard blank page

--- src/pages/Dashboard/TechTalent/Overview.jsx
+++ src/pages/Dashboard/TechTalent/Overview.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import {useContext, useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import "./profileMain.scss";
 import { SlBell } from "react-icons/sl";
@@ -18,6 +18,7 @@ function TechTalentOverview() {
   const user = useContext(UserContext);
   const navigate = useNavigate();
   const [success] = useState(false);
+  const [profileCompleted, setProfileCompleted] = useState(false);
 
   //getting nearby jobs and loggedInUser from the redux store
   const showNearByJobs = useSelector(
@@ -29,11 +30,18 @@ function TechTalentOverview() {
     (state) => state.LoggedInUserSlice.loggedInUser
   );
   console.log(nearByJobs);
-  const profileCompleted = loggedInUser.verified;
+  // const profileCompleted = loggedInUser.verified;
 
   //fetching recommended jobs
   const { data: recommendedJobs, loading: recommendedJobLoader } =
     useApiRequest(`/api/job-postings/${user.id}/recommend`);
+  useEffect(() => {
+    // Ensure profile completion status is updated based on loggedInUser
+    if (loggedInUser?.verified) {
+      setProfileCompleted(true);
+    }
+  }, [loggedInUser]);
+
 
   const openForm = (e) => {
     e.preventDefault();
