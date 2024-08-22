0592961d5b40517623ed2e4323793f5f7d374507
Joseph Nma <josgolf@yahoo.ca>
2024-08-22 13:50:49 +0100

    newsletter netlify submission.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -90,110 +90,150 @@ const Footer = () => {
                 your inbox.
               </p>
             </div>
+            {/*<form*/}
+            {/*  className="form-body"*/}
+            {/*  name="newsletter"*/}
+            {/*  method="POST"*/}
+            {/*  onSubmit={handleSubmit}>*/}
+            {/*   <input type="hidden" name='form-name' value='newsletter' />*/}
+            {/*  <div className="input-container" style={{ width: "100%" }}>*/}
+            {/*    {errors.email && (*/}
+            {/*      <p*/}
+            {/*        style={{*/}
+            {/*          color: "red",*/}
+            {/*          marginTop: "-.95rem",*/}
+            {/*          fontSize: ".8rem",*/}
+            {/*        }}>*/}
+            {/*        {errors.email}*/}
+            {/*      </p>*/}
+            {/*    )}*/}
+            {/*    <input*/}
+            {/*      type="email"*/}
+            {/*      name="email"*/}
+            {/*      autoComplete="off"*/}
+            {/*      id="email"*/}
+            {/*      placeholder="Email address"*/}
+            {/*      value={email}*/}
+            {/*      onChange={(e) => {*/}
+            {/*        setEmail(e.target.value);*/}
+            {/*        setErrors({ email: "" });*/}
+            {/*      }}*/}
+            {/*      className={*/}
+            {/*        errors.email === "setErrors" ? "error-input" : "sub-input"*/}
+            {/*      }*/}
+            {/*    />*/}
+            {/*  </div>*/}
+            {/*  <input type="submit" value="Submit" />*/}
+            {/*</form>*/}
             <form
-              className="form-body"
-              name="newsletter"
-              method="POST"
-              onSubmit={handleSubmit}>
-              {/* <input type="hidden" name='form-name' value='newsletter' /> */}
-              <div className="input-container" style={{ width: "100%" }}>
+                className="form-body"
+                name="newsletter"
+                method="POST"
+                data-netlify="true"
+                onSubmit={handleSubmit}
+            >
+              <input type="hidden" name="form-name" value="newsletter"/>
+              <div className="input-container" style={{width: "100%"}}>
                 {errors.email && (
-                  <p
-                    style={{
-                      color: "red",
-                      marginTop: "-.95rem",
-                      fontSize: ".8rem",
-                    }}>
-                    {errors.email}
-                  </p>
+                    <p
+                        style={{
+                          color: "red",
+                          marginTop: "-.95rem",
+                          fontSize: ".8rem",
+                        }}
+                    >
+                      {errors.email}
+                    </p>
                 )}
                 <input
-                  type="email"
-                  name="email"
-                  autoComplete="off"
-                  id="email"
-                  placeholder="Email address"
-                  value={email}
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                    setErrors({ email: "" });
-                  }}
-                  className={
-                    errors.email === "setErrors" ? "error-input" : "sub-input"
-                  }
+                    type="email"
+                    name="email"
+                    autoComplete="off"
+                    id="email"
+                    placeholder="Email address"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                      setErrors({email: ""});
+                    }}
+                    className={
+                      errors.email === "setErrors" ? "error-input" : "sub-input"
+                    }
                 />
               </div>
-              <input type="submit" value="Submit" />
+              <input type="submit" value="Submit"/>
             </form>
-            {isOpen && (
-              <Dialog
-                open={isOpen}
-                onClose={() => setIsOpen(false)}
-                style={{
-                  position: "fixed",
-                  left: "50%",
-                  top: "50%",
-                  transform: "translate(-50%, -50%)",
-                  width: "100%",
-                  maxWidth: "40rem",
-                  height: "25rem",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  background: "#ffffff",
-                  border: "0.06rem solid #d9d9d9",
-                  borderRadius: "30px",
-                  zIndex: "100",
-                }}>
-                <Dialog.Panel>
-                  <Dialog.Title
+
+            {isOpen
+                && (
+                <Dialog
+                    open={isOpen}
+                    onClose={() => setIsOpen(false)}
                     style={{
-                      fontFamily: "Manrope",
-                      marginBottom: "2rem",
-                      color: "#000000",
-                      textAlign: "center",
+                      position: "fixed",
+                      left: "50%",
+                      top: "50%",
+                      transform: "translate(-50%, -50%)",
+                      width: "100%",
+                      maxWidth: "40rem",
+                      height: "25rem",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      background: "#ffffff",
+                      border: "0.06rem solid #d9d9d9",
+                      borderRadius: "30px",
+                      zIndex: "100",
                     }}>
-                    <div>
-                      <h2
+                  <Dialog.Panel>
+                    <Dialog.Title
                         style={{
-                          fontSize: "2rem",
-                          fontWeight: "700",
-                          lineHeight: "2.5rem",
-                          color: "#2596BE",
-                          marginBottom: "1.5rem",
-                        }}>
-                        Thank You !
-                      </h2>
-                      <p
-                        style={{
-                          fontSize: "1rem",
-                          fontWeight: "400",
-                          lineHeight: "22px",
-                        }}>
-                        `Thank you for subscribing to the newsletter.
-                        <br />
-                        You should receive a confirmation email soon.`
-                      </p>
-                    </div>
-                    <div style={{ marginTop: "3rem" }}>
-                      <Link
-                        style={{
-                          background: "#2596BE",
-                          padding: "10px",
-                          display: "block",
-                          color: "#ffffff",
-                          borderRadius: "0.6rem",
+                          fontFamily: "Manrope",
+                          marginBottom: "2rem",
+                          color: "#000000",
                           textAlign: "center",
-                          fontSize: "1rem",
-                          fontWeight: "400",
-                        }}
-                        to={"/login"}>
-                        Login to your account
-                      </Link>
-                    </div>
-                  </Dialog.Title>
-                </Dialog.Panel>
-              </Dialog>
+                        }}>
+                      <div>
+                        <h2
+                            style={{
+                              fontSize: "2rem",
+                              fontWeight: "700",
+                              lineHeight: "2.5rem",
+                              color: "#2596BE",
+                              marginBottom: "1.5rem",
+                            }}>
+                          Thank You !
+                        </h2>
+                        <p
+                            style={{
+                              fontSize: "1rem",
+                              fontWeight: "400",
+                              lineHeight: "22px",
+                            }}>
+                          `Thank you for subscribing to the newsletter.
+                          <br/>
+                          You should receive a confirmation email soon.`
+                        </p>
+                      </div>
+                      <div style={{marginTop: "3rem"}}>
+                        <Link
+                            style={{
+                              background: "#2596BE",
+                              padding: "10px",
+                              display: "block",
+                              color: "#ffffff",
+                              borderRadius: "0.6rem",
+                              textAlign: "center",
+                              fontSize: "1rem",
+                              fontWeight: "400",
+                            }}
+                            to={"/login"}>
+                          Login to your account
+                        </Link>
+                      </div>
+                    </Dialog.Title>
+                  </Dialog.Panel>
+                </Dialog>
             )}
           </div>
           <div className="footer-social-acct">
@@ -201,26 +241,26 @@ const Footer = () => {
             <div className="media-handles">
               <div className="facebk">
                 <Link
-                  to={
-                    "https://www.facebook.com/nxgjobhub"
-                  }
-                  target="_blank">
+                    to={
+                      "https://www.facebook.com/nxgjobhub"
+                    }
+                    target="_blank">
                   <BiLogoFacebook
-                    style={{
-                      width: "1.1rem",
-                      height: "1.1rem",
-                      color: "#2596be",
-                      marginTop: ".3rem",
-                    }}
+                      style={{
+                        width: "1.1rem",
+                        height: "1.1rem",
+                        color: "#2596be",
+                        marginTop: ".3rem",
+                      }}
                   />
                 </Link>
               </div>
               <div className="google">
                 <Link to={"#"}>
                   <BiLogoGoogle
-                    style={{
-                      width: "1.1rem",
-                      height: "1.1rem",
+                      style={{
+                        width: "1.1rem",
+                        height: "1.1rem",
                       color: "#2596be",
                       marginTop: ".3rem",
                     }}
