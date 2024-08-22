da11faf03607b4ccb7fc9f809dfd0b04d95c17ff
Joseph Nma <josgolf@yahoo.ca>
2024-08-22 09:53:35 +0100

    state input for tech talent fix.

--- src/pages/Dashboard/TechTalent/DashboardProfileForm/index.jsx
+++ src/pages/Dashboard/TechTalent/DashboardProfileForm/index.jsx
@@ -14,8 +14,8 @@ function TechTalentProfileForm() {
   const [formData, setFormData] = useState({
     countryCode: "",
     zipCode: "",
-    residentialAddress: "",
     state: "",
+    residentialAddress: "",
     highestQualification: "",
     professionalCert: "",
     experienceLevel: "",
@@ -79,8 +79,8 @@ function TechTalentProfileForm() {
     const formErrors = {};
     if (
       formData.countryCode === "" ||
-      formData.zipCode === ""
-      // formData.state === ""
+      // formData.zipCode === ""
+      formData.state === ""
     ) {
       formErrors.formData = "All fields must be filled";
       setErrors(formErrors);
