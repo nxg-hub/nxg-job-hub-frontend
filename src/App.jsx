e16c4bc840faec1993ebe3ed6ad7359123effc93
Joseph Nma <josgolf@yahoo.ca>
2025-05-19 09:58:51 +0100

    optimize imports for deployment.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "@/pages/Home.jsx";
-import ProfileLanding from "@/pages/ProfileLanding.jsx";
-import Services from "@/pages/Services.jsx";
-import About from "@/pages/About.jsx";
-import Contact from "@/pages/Contact.jsx";
-import Explore from "@/components/hero/Explore.jsx";
-import Dashboard from "@/pages/Dashboard/index.jsx";
-import EmployerProfileForm from "@/pages/Dashboard/Employer/EmployerProfile/index.jsx";
-import Passwordsettings from "@/pages/Dashboard/TechTalent/setting/Passwordsettings.jsx";
-import DashboardProfileForm from "@/pages/Dashboard/TechTalent/DashboardProfileForm/index.jsx";
+import Home from "./pages/Home.jsx";
+import ProfileLanding from "./pages/ProfileLanding.jsx";
+import Services from "./pages/Services.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import Explore from "./components/hero/Explore.jsx";
+import Dashboard from "./pages/Dashboard/index.jsx";
+import EmployerProfileForm from "./pages/Dashboard/Employer/EmployerProfile/index.jsx";
+import Passwordsettings from "./pages/Dashboard/TechTalent/setting/Passwordsettings.jsx";
+import DashboardProfileForm from "./pages/Dashboard/TechTalent/DashboardProfileForm/index.jsx";
 // import Wallet from "./pages/Dashboard/wallet/Wallet.jsx";
 import JobListings from "./pages/Dashboard/job-listings";
 import Login from "./pages/Login/index.jsx";
@@ -20,58 +20,58 @@ import PostJobs from "./pages/Dashboard/Employer/routes/PostJobs/index.jsx";
 import NotificationTab from "./components/NotificationTab/index.jsx";
 import JobPosts from "./pages/Dashboard/Employer/routes/JobPosts/index.jsx";
 // import JobApplicants from "./pages/Dashboard/Employer/routes/JobApplicants/index.jsx";
-import EmployerVerificationForm from "@/pages/Dashboard/Employer/routes/EmployerDashProfile/EmployerVerificationForm.jsx";
-import ProfileController from "@/utils/routers/ProfileController.jsx";
-import Overview from "@/utils/routers/Overview.jsx";
-import SubscriptionController from "@/utils/routers/SubscriptionController.jsx";
-import { VerificationProvider } from "@/pages/Dashboard/Employer/routes/EmployerDashProfile/VerificationContext.jsx";
-import ProtectedRoute from "@/utils/routers/ProtectedRoute.jsx";
-import SavedJobCard from "@/pages/Dashboard/TechTalent/SavedJobs/SavedJobCard/index.jsx";
-import { SubSuccess } from "@/pages/Dashboard/subscriptions/SubSuccess.jsx";
-import Findjob from "@/pages/FindJob/FindjobPage.jsx";
-import Faq from "@/pages/Faq/Faq.jsx";
-import PrivacyPolicy from "@/pages/Privacy/Privacy.jsx";
-import TermsAndCondition from "@/pages/TermsAndCondition/TermsAndCondition.jsx";
-import Help from "@/pages/Dashboard/Help/Help.jsx";
-import SavedJobs from "@/pages/Dashboard/TechTalent/SavedJobs/index.jsx";
-import Interview from "@/pages/Dashboard/Employer/routes/Interview/Interview.jsx";
-import CompanyServices from "@/pages/Dashboard/Employer/routes/Mycompanyservices/CompanyServices.jsx";
-import ReviewApplicants from "@/pages/Dashboard/Employer/routes/JobApplicants/reviewApplicant/ReviewApplicants.jsx";
-import FullReview from "@/pages/Dashboard/Employer/routes/JobApplicants/reviewApplicant/FullReview.jsx";
-import Applications from "@/pages/Dashboard/TechTalent/ApplicationCard/index.jsx";
-import FeaturedTalent from "@/pages/Dashboard/FeaturedTalent/index.jsx";
-import PostJobForm from "@/pages/PostJobForm.jsx";
-import SuccessfulJobPost from "@/components/ExternalJobPost/SuccessfulJobPost.jsx";
-import LoginForm from "@/pages/Login/Login.jsx";
-import SignupForm from "@/pages/Register/SignupPage.jsx";
-import { ServiceProviderDashboard } from "@/pages/services-provider.jsx";
-import { ServicesProviderHomePage } from "@/pages/Dashboard/ServiceProvider/dashboardhome.jsx";
-import ServiceProviderProfile from "@/pages/Dashboard/ServiceProvider/profile.jsx";
-import { ServiceProviderFormCompletion } from "@/pages/servicesprovidercompleteform.jsx";
-import { JobTracker } from "@/pages/Dashboard/ServiceProvider/jobs-tracker.jsx";
-import MessagesPage from "@/pages/Dashboard/messages.jsx";
-import { AgentDashboard } from "@/pages/agent.jsx";
-import DashboardTab from "@/pages/Dashboard/Agent/dashboard-tab.jsx";
-import MatchesTab from "@/pages/Dashboard/Agent/matches-tab.jsx";
-import JobsTab from "@/pages/Dashboard/Agent/jobs-tab.jsx";
-import CandidatesTab from "@/pages/Dashboard/Agent/candidates-tabs.jsx";
-import MessagesTab from "@/pages/Dashboard/Agent/messages-tab.jsx";
-import ProfileTab from "@/pages/Dashboard/Agent/profile-tab.jsx";
-import EmployerTab from "@/pages/Dashboard/Agent/employers-tab.jsx";
-import { EmployerDashboard } from "@/pages/employer.jsx";
-import EmployerDashboardTab from "@/pages/Dashboard/Employer2/employer-dashboard-tab.jsx.jsx";
-import EmployerJobTab from "@/pages/Dashboard/Employer2/employer-job-tab.jsx";
-import EmployerCompanyProfileTab from "@/pages/Dashboard/Employer2/employer-company-profile-tab.jsx";
-import EmployerApplicantsTab from "@/pages/Dashboard/Employer2/employer-applicants-tab.jsx";
+import EmployerVerificationForm from "./pages/Dashboard/Employer/routes/EmployerDashProfile/EmployerVerificationForm.jsx";
+import ProfileController from "./utils/routers/ProfileController.jsx";
+import Overview from "./utils/routers/Overview.jsx";
+import SubscriptionController from "./utils/routers/SubscriptionController.jsx";
+import { VerificationProvider } from "./pages/Dashboard/Employer/routes/EmployerDashProfile/VerificationContext.jsx";
+import ProtectedRoute from "./utils/routers/ProtectedRoute.jsx";
+import SavedJobCard from "./pages/Dashboard/TechTalent/SavedJobs/SavedJobCard/index.jsx";
+import { SubSuccess } from "./pages/Dashboard/subscriptions/SubSuccess.jsx";
+import Findjob from "./pages/FindJob/FindjobPage.jsx";
+import Faq from "./pages/Faq/Faq.jsx";
+import PrivacyPolicy from "./pages/Privacy/Privacy.jsx";
+import TermsAndCondition from "./pages/TermsAndCondition/TermsAndCondition.jsx";
+import Help from "./pages/Dashboard/Help/Help.jsx";
+import SavedJobs from "./pages/Dashboard/TechTalent/SavedJobs/index.jsx";
+import Interview from "./pages/Dashboard/Employer/routes/Interview/Interview.jsx";
+import CompanyServices from "./pages/Dashboard/Employer/routes/Mycompanyservices/CompanyServices.jsx";
+import ReviewApplicants from "./pages/Dashboard/Employer/routes/JobApplicants/reviewApplicant/ReviewApplicants.jsx";
+import FullReview from "./pages/Dashboard/Employer/routes/JobApplicants/reviewApplicant/FullReview.jsx";
+import Applications from "./pages/Dashboard/TechTalent/ApplicationCard/index.jsx";
+import FeaturedTalent from "./pages/Dashboard/FeaturedTalent/index.jsx";
+import PostJobForm from "./pages/PostJobForm.jsx";
+import SuccessfulJobPost from "./components/ExternalJobPost/SuccessfulJobPost.jsx";
+import LoginForm from "./pages/Login/Login.jsx";
+import SignupForm from "./pages/Register/SignupPage.jsx";
+import { ServiceProviderDashboard } from "./pages/services-provider.jsx";
+import { ServicesProviderHomePage } from "./pages/Dashboard/ServiceProvider/dashboardhome.jsx";
+import ServiceProviderProfile from "./pages/Dashboard/ServiceProvider/profile.jsx";
+import { ServiceProviderFormCompletion } from "./pages/servicesprovidercompleteform.jsx";
+import { JobTracker } from "./pages/Dashboard/ServiceProvider/jobs-tracker.jsx";
+import MessagesPage from "./pages/Dashboard/messages.jsx";
+import { AgentDashboard } from "./pages/agent.jsx";
+import DashboardTab from "./pages/Dashboard/Agent/dashboard-tab.jsx";
+import MatchesTab from "./pages/Dashboard/Agent/matches-tab.jsx";
+import JobsTab from "./pages/Dashboard/Agent/jobs-tab.jsx";
+import CandidatesTab from "./pages/Dashboard/Agent/candidates-tabs.jsx";
+import MessagesTab from "./pages/Dashboard/Agent/messages-tab.jsx";
+import ProfileTab from "./pages/Dashboard/Agent/profile-tab.jsx";
+import EmployerTab from "./pages/Dashboard/Agent/employers-tab.jsx";
+import { EmployerDashboard } from "./pages/employer.jsx";
+import EmployerDashboardTab from "./pages/Dashboard/Employer2/employer-dashboard-tab.jsx.jsx";
+import EmployerJobTab from "./pages/Dashboard/Employer2/employer-job-tab.jsx";
+import EmployerCompanyProfileTab from "./pages/Dashboard/Employer2/employer-company-profile-tab.jsx";
+import EmployerApplicantsTab from "./pages/Dashboard/Employer2/employer-applicants-tab.jsx";
 
-import EmployerSettingTab from "@/pages/Dashboard/Employer2/employer-setting-tab.jsx";
+import EmployerSettingTab from "./pages/Dashboard/Employer2/employer-setting-tab.jsx";
 import EmployerMessageTab from "./pages/Dashboard/Employer2/Employer-message-tab.jsx";
-import EmployerMessagesTab from "/src/pages/Dashboard/Employer2/employer-message-tab.jsx";
-import { TalentDashboard } from "@/pages/talent";
-import TalentDashboardTab from "@/pages/Dashboard/Talent/Talent-Dashboard-Tab.jsx";
-import TalentProfileTab from "@/pages/Dashboard/Talent/talent-profile-tab.jsx";
-import TalentJobsTab from "@/pages/Dashboard/Talent/talent-jobs-tab.jsx";
-import TalentMessageTab from "@/pages/Dashboard/Talent/talent-message-tab.jsx";
+// import EmployerMessagesTab from "./pages/Dashboard/Employer2/Employer-message-tab.jsx";
+import { TalentDashboard } from "./pages/talent.jsx";
+import TalentDashboardTab from "./pages/Dashboard/Talent/Talent-Dashboard-Tab.jsx";
+import TalentProfileTab from "./pages/Dashboard/Talent/talent-profile-tab.jsx";
+import TalentJobsTab from "./pages/Dashboard/Talent/talent-jobs-tab.jsx";
+import TalentMessageTab from "./pages/Dashboard/Talent/talent-message-tab.jsx";
 
 
 function App() {
@@ -116,10 +116,10 @@ function App() {
             path="applicants"
             element={<EmployerApplicantsTab />}
           />
-          <Route
-            path="messages"
-            element={<EmployerMessagesTab />}
-          />
+          {/*<Route*/}
+          {/*  path="messages"*/}
+          {/*  element={<EmployerMessagesTab />}*/}
+          {/*/>*/}
           
           <Route
             path="setting"
