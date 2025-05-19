e12ccbc8e2a1e45e9de66c428bd09bd149c0f0f6
Joseph Nma <josgolf@yahoo.ca>
2025-05-19 10:00:40 +0100

    optimize imports for deployment.

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ import EmployerCompanyProfileTab from "./pages/Dashboard/Employer2/employer-comp
 import EmployerApplicantsTab from "./pages/Dashboard/Employer2/employer-applicants-tab.jsx";
 
 import EmployerSettingTab from "./pages/Dashboard/Employer2/employer-setting-tab.jsx";
-import EmployerMessageTab from "./pages/Dashboard/Employer2/Employer-message-tab.jsx";
+// import EmployerMessageTab from "./pages/Dashboard/Employer2/Employer-message-tab.jsx";
 // import EmployerMessagesTab from "./pages/Dashboard/Employer2/Employer-message-tab.jsx";
 import { TalentDashboard } from "./pages/talent.jsx";
 import TalentDashboardTab from "./pages/Dashboard/Talent/Talent-Dashboard-Tab.jsx";
