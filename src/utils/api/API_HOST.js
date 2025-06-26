134ef5d6f96b21626780eef21e22e71a45b7c7a1
Joseph Nma <josgolf@yahoo.ca>
2025-06-26 12:29:38 +0100

    adjust production build.

--- src/utils/api/API_HOST.js
+++ src/utils/api/API_HOST.js
@@ -2,7 +2,7 @@
 
 
 
-export const API_HOST_URL = "https://job-hub-91sr.onrender.com"
+export const API_HOST_URL = "https://nxg-job-hub-backend-hv38.onrender.com"
 
 // export const API_HOST_URL = "https://nxg-job-hub-backend.onrender.com";
 
