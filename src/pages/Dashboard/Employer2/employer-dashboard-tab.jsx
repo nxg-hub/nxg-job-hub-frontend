ecc624db95924948f3aa1a00fe6d7774f6267e90
Joseph Nma <josgolf@yahoo.ca>
2025-06-26 12:42:00 +0100

    adjust production build.

--- src/pages/Dashboard/Employer2/employer-dashboard-tab.jsx
+++ src/pages/Dashboard/Employer2/employer-dashboard-tab.jsx
@@ -11,7 +11,7 @@ import {
 } from "lucide-react";
 
 import { cn } from "@/lib/utils";
-import sarahicon from "@/static/images/john.png";
+import sarahicon from "@/static/images/John.png";
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
