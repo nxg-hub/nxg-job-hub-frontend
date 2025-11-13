84dd61e4169a98c589b6c9fd2dffee63e2cdb445
kenesongodwin@gmail.com <kenesongodwin@gmail.com>
2025-11-13 13:24:46 +0100

    corrections

--- src/pages/FindJob/FindjobPage.jsx
+++ src/pages/FindJob/FindjobPage.jsx
@@ -29,6 +29,7 @@ const FindjobPage = () => {
         );
         if (!response.ok) throw new Error(`Error ${response.status}`);
         const data = await response.json();
+        console.log(data);
         const acceptedData = data.filter((job) => job.jobStatus === "ACCEPTED");
         setJobsResult(acceptedData);
       } catch (error) {
